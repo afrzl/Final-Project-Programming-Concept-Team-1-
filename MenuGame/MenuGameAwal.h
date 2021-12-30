467c45b534615eab1038bb7ca0f29cc10613ccf8
xazhm <azzamhilme@gmail.com>
2021-12-31 02:17:29 +0700

    Menu Awalan Snake [Setting IMG + FONT Before Use]

---
+++ MenuGame/MenuGameAwal.h
@@ -0,0 +1,26 @@
+#include <SFML/Graphics.hpp>
+#define Max_main_menu 3
+
+class Menuu
+{
+
+public:
+	Menuu(float width, float height);
+
+	void draw(sf::RenderWindow &window);
+	void moveUp();
+	void moveDown();
+
+	int mainMenuPressed()
+	{
+		return mainMenuSelected;
+	}
+
+	~Menuu();
+
+private:
+	int mainMenuSelected;
+	sf::Font font;
+	sf::Text mainMenu[Max_main_menu];
+};
+
